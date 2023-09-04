d146bc48a28a1d413a5894a2dc37b4d8eff1df8e
Mark <zwill_1@hotmail.com>
2023-09-03 23:10:15 -0600

    Adding routes and layout pages

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Testing admin routes
+
+### Tech Stack
+
+Tailwind CSS
+
+React-router-dom ~ routing service (https://reactrouter.com/en/main)
+
+eslint
+
+Material SVG icons ~ icons (https://mui.com/material-ui/material-icons/)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./styles/index.css";
+
+// page imports
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+// admin page import
+import AppLayout from "./pages/admin/AppLayout";
+import ProtectedRoute from "./pages/admin/ProtectedRoute";
+
+// routing
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* Children routes are wrapped in a parent route */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+
+        {/* Admin protected route. Cant do this in 6.0 as protected route doesnt exist yet */}
+        <Route
+          path="app"
+          element={
+            <ProtectedRoute>
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
