2b645c1c0976b50160189ee2fff74196298b1880
Mark <zwill_1@hotmail.com>
2023-09-08 00:32:32 -0600

    adding components to admin

---
+++ src/components/admin/AccordianList.jsx
@@ -0,0 +1,3 @@
+export default function AccordianList() {
+  return <div>AccordianList</div>;
+}
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,13 +1,21 @@
+import AdminNav from "../../components/admin/AdminNav";
+import AccordianList from "../../components/admin/AccordianList";
+import PieChart from "../../components/admin/PieChart";
+
 export default function AdminDashboard() {
   return (
     <>
       <section className="w-full flex flex-wrap flex-row min-h-screen bg-gray-600">
         <div className="w-full xl:w-2/12 text-center pt-4 bg-gray-300">
-          Dashboard Navigation
+          <AdminNav />
         </div>
         <div className="w-full xl:w-10/12 text-center flex flex-wrap">
-          <div className="xl:w-6/12 text-center bg-gray-100">1</div>
-          <div className="xl:w-6/12 text-center bg-gray-200">2</div>
+          <div className="xl:w-6/12 text-center bg-gray-100">
+            <AccordianList />
+          </div>
+          <div className="xl:w-6/12 text-center bg-gray-200">
+            <PieChart />
+          </div>
           <div className="xl:w-6/12 text-center bg-gray-500">3</div>
           <div className="xl:w-6/12 text-center bg-gray-600">4</div>
         </div>
