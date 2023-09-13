1a6bed51dec931b71ebfacfbd906fa70d499958b
Mark <zwill_1@hotmail.com>
2023-09-13 01:03:04 -0600

    Setting up axios to get data

--- src/components/admin/AccordianList.jsx
+++ src/components/admin/AccordianList.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import data from "../admin/Data/AccordianListData";
+
 export default function AccordianList() {
+  const [apiData, setApiData] = useState(null);
+
+  // When getting data, set useeffect to "loading".
+
+  useEffect(() => {
+    axios.get("src/components/admin/Data/AccordianListData.js").then((res) => {
+      setApiData(res.data);
+
+      // Need to catch any errors if cannot get API data.
+
+      // Set "loading" to false.
+    });
+  }, []);
+  console.log("Running this outside use effect. " + apiData);
+
   return (
     <>
       <section>
