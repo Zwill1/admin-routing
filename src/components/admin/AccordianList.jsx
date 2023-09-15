68395308ebea921e941ac4497b3215336f691ff9
Mark <zwill_1@hotmail.com>
2023-09-15 00:51:34 -0600

    Setting up json data to use with dashboard

--- src/components/admin/AccordianList.jsx
+++ src/components/admin/AccordianList.jsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import useFetch from "./hooks/useFetch";
 
 export default function AccordianList() {
-  const [apiData, setApiData] = useState("");
+  const { data, loading, error } = useFetch(
+    "src/components/admin/Data/AccordianListData.json"
+  );
 
-  // When getting data, set useeffect to "loading".
+  // When getting data, set to "loading".
+  if (loading) return <h1>Loading data...</h1>;
 
-  useEffect(() => {
-    axios.get("src/components/admin/Data/AccordianListData.js").then((res) => {
-      setApiData(res.data);
+  if (data === null)
+    return <div>No data available. Please try again later.</div>;
 
-      // Need to catch any errors if cannot get API data.
+  // Return error in console if one shows up
+  if (error) console.log("there is an error " + error);
 
-      // Set "loading" to false.
-    });
-  }, []);
-  console.log("Running this outside use effect. " + apiData);
+  console.log("this is the data ", data);
+  console.log("testing data " + data);
 
   return (
     <>
       <section>
-        <div className="flex justify-center bg-gray-500 p-4 mt-2 mb-2">
-          <p className="p-2">Image thumbnail</p>
-          <div className="p-2">Accordian List Info</div>
-        </div>
-        <div className="flex justify-center bg-gray-500 p-4 mt-2 mb-2">
-          <p className="p-2">Image thumbnail</p>
-          <div className="p-2">Accordian List Info</div>
-        </div>
-        <div className="flex justify-center bg-gray-500 p-4 mt-2 mb-2">
-          <p className="p-2">Image thumbnail</p>
-          <div className="p-2">Accordian List Info</div>
-        </div>
-        <div className="flex justify-center bg-gray-500 p-4 mt-2 mb-2">
-          <p className="p-2">Image thumbnail</p>
-          <div className="p-2">Accordian List Info</div>
-        </div>
+        {data &&
+          data.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-center bg-gray-500 p-4 mt-2 mb-2"
+            >
+              <p className="p-2">{item.id}</p>
+              <div className="p-2">
+                {item.car_brand} / {item.car_model}
+              </div>
+            </div>
+          ))}
       </section>
     </>
   );
