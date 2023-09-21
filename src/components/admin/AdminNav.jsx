023e5fb4960fc7b088fea67559bf304c9aaaa47b
zwill <zwill_1@hotmail.com>
2023-09-20 23:40:34 -0600

    adding user name and avatar to project

--- src/components/admin/AdminNav.jsx
+++ src/components/admin/AdminNav.jsx
@@ -5,12 +5,19 @@ import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function AdminNav() {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  console.log(user);
 
   return (
     <>
       <div className="bg-gray-600 text-white p-4">Dashboard Navigation</div>
       <ul className="pr-4">
+        <li className="px-4 pt-4">
+            <img src={user.avatar} />
+        </li>
+        <li className="px-4">
+            Welcome, {user.name}
+        </li>
         <li className="p-4">
           <Link to="/" className="nav-link">
             <HomeIcon /> Home
